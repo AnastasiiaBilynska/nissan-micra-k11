3d2c6079b9612d2247a5590f84ee6bca7c122512
AnastasiiaBilynska <nk015132@gmail.com>
2021-02-22 16:25:55 +0200

    add_carousel(it_doesnt_work)

--- js/index.js
+++ js/index.js
@@ -17,3 +17,35 @@ document.querySelector(".header_burger").addEventListener("click", () => (cap, h
  });
 
  document.querySelector(".header_burger")*/
+
+
+
+
+class Carousel {
+  constructor("products") {
+    this.product = product;
+    this.currInd = 0;
+    this.showCurrentProduct();
+    document.querySelector(".prev").addEventListener("click",
+      () => this.showPrevProduct());
+    document.querySelector(".next").addEventListener("click",
+      () => this.nextProduct());
+    setInterval(() => this.nextProduct(), 5000);
+  }
+  showCurrentProduct() {
+    document.querySelector(".current") = this.nextProduct[this / currInd];
+  }
+  nextProduct() {
+    this.currInd++;
+    if (++this.currInd >= this.product.length) this.currInd = 0;
+    this.showCurrentProduct();
+  }
+  prevProduct() {
+    if (--this.currInd < 0) this.currInd = this.product.length - 1;
+    this.showCurrentProduct();
+  }
+}
+
+new Carousel([document.querySelectorAll("li .carousel"]);
+
+
