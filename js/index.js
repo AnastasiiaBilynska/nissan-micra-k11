49b8402a4be9a84170d4fad33a481e32cc959712
AnastasiiaBilynska <nk015132@gmail.com>
2021-02-22 14:59:25 +0200

    correctinon

--- js/index.js
+++ js/index.js
@@ -3,3 +3,17 @@ function updateClock() {
   const clock = document.querySelector(".time");
   clock.innerText = new Date().toLocaleTimeString();
 }
+/*function openBurger() {
+const burger = document.querySelector(".header_burger");
+const cap = document.querySelector(".cap");
+const headerMenu = document.querySelector(".header_nav");
+
+document.querySelector(".header_burger").addEventListener("click", () => (cap, headerMenu).toggleClass("active");  )
+} */
+/*$(document).ready(function() {
+  $(".header_burger").click(function(event) {
+   $(".heder_burger, .cap, .header_nav").toggleClass("active");
+  });
+ });
+
+ document.querySelector(".header_burger")*/
