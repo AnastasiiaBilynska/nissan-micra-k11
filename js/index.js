7eed3529ef746a109d490ddbc441ae3302e3606e
AnastasiiaBilynska <nk015132@gmail.com>
2021-02-24 00:24:01 +0200

    add burger

--- js/index.js
+++ js/index.js
@@ -3,6 +3,21 @@ function updateClock() {
   const clock = document.querySelector(".time");
   clock.innerText = new Date().toLocaleTimeString();
 }
+
+document.querySelector(".header_burger").addEventListener("click", function(e) {
+  e.preventDefault();
+ // this.classList.toggle("is-active");
+ if(this.classList.contains("is-active")) {
+  this.classList.remove("is-active");
+  document.querySelector(".js_menu").classList.remove("active");
+ }
+ else {
+   this.classList.add("is-active");
+   document.querySelector(".js_menu").classList.add("active");
+ }
+})
+
+
 /*function openBurger() {
 const burger = document.querySelector(".header_burger");
 const cap = document.querySelector(".cap");
